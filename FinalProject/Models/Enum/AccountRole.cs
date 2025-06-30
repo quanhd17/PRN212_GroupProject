d38f2dfa3477dc1d5e1d565dcf722ae139f93ed5
quanhd17 <dongquan0123@gmail.com>
2025-06-30 15:12:28 +0700

    adding login and admin/staff pages

---
+++ FinalProject/Models/Enum/AccountRole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.Models.Enum
+{
+    public enum AccountRole
+    {
+        Admin = 0,
+        Staff = 1
+    }
+}
---
+++ FinalProject/ViewModels/LoginViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject.ViewModels
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Input;
+    using FinalProject.Models;
+    using Microsoft.EntityFrameworkCore;
+    using FinalProject.ViewModels.Helpers;
+
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set { _username = value; OnPropertyChanged(); }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set { _password = value; OnPropertyChanged(); }
+        }
+
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(Login);
+        }
+
+        private void Login()
+        {
+            using (var db = new Prn212DbContext())
+            {
+                var user = db.Accounts
+                             .FirstOrDefault(a => a.Username == Username && a.Password == Password);
+
+                if (user == null)
+                {
+                    MessageBox.Show("Invalid username or password.");
+                    return;
+                }
+
+                if (user.IsBanned == true)
+                {
+                    MessageBox.Show("Account has been locked.");
+                    return;
+                }
+
+                switch (user.Role)
+                {
+                    case 0:
+                        MessageBox.Show("Hello Admin: " + user.FullName);
+                        break;
+                    case 1:
+                        MessageBox.Show("Hello Staff: " + user.FullName);
+                        break;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
